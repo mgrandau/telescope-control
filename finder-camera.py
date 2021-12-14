1072ad1469e99898ab6c2c32c5dce14294ac4285
mgrandau <mgrandau@gmail.com>
2021-12-13 19:07:10 -0600

    Telerad overlay for finder-camera

--- finder-camera.py
+++ finder-camera.py
@@ -8,15 +8,12 @@ import threading
 
 class VideoCamera(object):
     _video = None
-    _exposure = 22000000
-    _offset_cross = (-12,-8)
-    _gain = 82
     last_image: Optional[bytes] = None
 
     def __init__(self):
         self._video = asi.Camera(0)
         self._video.setCaptureFrameFormat(1280, 960, 1, "RGB24")
-        self._video.setControlValueManual("ASI_GAIN", self._gain)
+        self._video.setControlValueManual("ASI_GAIN", 82)
         self._video.setControlValueManual("ASI_WB_R", 55)
         self._video.setControlValueManual("ASI_WB_B", 77)
 
@@ -24,18 +21,14 @@ class VideoCamera(object):
         del self._video
 
     def get_frame(self) -> bytes:
-        self.last_image, bin, success = self._video.grab(self._exposure)
+        self.last_image, bin, success = self._video.grab(22000000)
+
         b,g,r = cv2.split(self.last_image)
         corrected_image = cv2.merge ( (r, g, b) )
-        #print(self.last_image.shape)
-        png = corrected_image[80-self._offset_cross[0]:880-self._offset_cross[0],240-self._offset_cross[1]:1040-self._offset_cross[1]]
-        #print(png.shape)
-        #ret, png = cv2.imencode('.png', self.last_image)
 
-        cross = cv2.imread('overlays/alignment-cross-hair.png')
-        #print(cross.shape)
-        #overlay = cv2.imencode('.png',cv2.imread('cross-hair.png'))
-        added_image = cv2.addWeighted(png,1,cross,1,0,dtype = cv2.CV_32F)
+        overlay = cv2.imread('overlays/telerad-overlay.png')
+        
+        added_image = cv2.addWeighted(corrected_image,1,overlay,1,0,dtype = cv2.CV_32F)
         
         ret, jpeg = cv2.imencode('.jpg', added_image)
         return jpeg.tobytes()
